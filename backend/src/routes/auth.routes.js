63c842956a4bd666fad3fda77b1e8c8c1982bf34
Utsav-Bhattarai-code <featherfeather3@gmail.com>
2026-02-28 17:13:49 +0545

    auth-route-setup

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+
+// Building a router for better management
+const router = express.Router();
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,9 @@
 import express from "express";
 
 //importing otther files for usuage
-import { ENV } from "./lib/ENV";
+import { ENV } from "./lib/ENV.js";
+import { connectDB } from "./lib/connectDB.js";
+import authRoutes from "./routes/auth.routes.js"
 
 // making main app
 const app = express();
@@ -10,7 +12,11 @@ const app = express();
 // using standard middlewares
 app.use(express.json());
 
-// Running the app on local port
+// using routers
+app.use("./api/auth" , authRoutes)
+
+// running the app on local port
 app.listen(ENV.PORT , () => {
     console.log("Server is running at port :",ENV.PORT);
+    connectDB();
 })
